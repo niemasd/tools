3b522667d21ba3a1c75c95c43ef7c5a8671b46b8
Niema Moshiri <niemamoshiri@gmail.com>
2016-10-15 20:17:01 -0700

    Added functions

--- numlist.cpp
+++ numlist.cpp
@@ -14,21 +14,23 @@ using namespace std;
 // usage message
 const string USAGE_MESSAGE =
 "\nUSAGE: numlist -ARG\n"
-"    -avg:       Compute average of list of numbers\n"
-"    -csv:       Print the list as comma-separated values\n"
-"    -div<NUM>:  Divide all numbers by <NUM>\n"
-"    -gt<NUM>:   Print all numbers greater than <NUM>\n"
-"    -gte<NUM>:  Print all numbers greater than or equal to <NUM>\n"
-"    -int:       Print the list as integers\n"
-"    -lt<NUM>:   Print all numbers less than <NUM>\n"
-"    -lte<NUM>:  Print all numbers less than or equal to <NUM>\n"
-"    -max:       Compute maximum of list of numbers\n"
-"    -min:       Compute minimum of list of numbers\n"
-"    -mult<NUM>: Multiply all numbers by <NUM>\n"
-"    -sum:       Compute sum of list of numbers\n";
+"    -avg:        Compute average of list of numbers\n"
+"    -csv:        Print the list as comma-separated values\n"
+"    -div<NUM>:   Divide all numbers by <NUM>\n"
+"    -dlm'<STR>': Print the list, but delimited by <STR>\n"
+"    -gt<NUM>:    Print all numbers greater than <NUM>\n"
+"    -gte<NUM>:   Print all numbers greater than or equal to <NUM>\n"
+"    -int:        Print the list as integers\n"
+"    -lt<NUM>:    Print all numbers less than <NUM>\n"
+"    -lte<NUM>:   Print all numbers less than or equal to <NUM>\n"
+"    -max:        Compute maximum of list of numbers\n"
+"    -min:        Compute minimum of list of numbers\n"
+"    -mult<NUM>:  Multiply all numbers by <NUM>\n"
+"    -sum:        Compute sum of list of numbers\n"
+"    -tsv:        Print the list as tab-separated values\n";
 
 // compute the sum of a list of numers
-double sum( vector<double> & nums ) {
+double sum( const vector<double> & nums ) {
     double out = 0;
     for(auto i : nums) {
         out += i;
@@ -37,28 +39,38 @@ double sum( vector<double> & nums ) {
 }
 
 // compute the average of a list of numbers
-double avg( vector<double> & nums ) {
+double avg( const vector<double> & nums ) {
     return sum(nums) / nums.size();
 }
 
-// print the list of numbers as comma-separated
-void csv( vector<double> & nums ) {
+// delimit the numbers using delimiter
+void dlm( const vector<double> & nums, const string & delimiter) {
     cout << nums[0];
     for(int i = 1; i < nums.size(); ++i) {
-        cout << ',' << nums[i];
+        cout << delimiter << nums[i];
     }
     cout << endl;
 }
 
+// print the list of numbers as comma-separated
+void csv( const vector<double> & nums ) {
+    dlm(nums,",");
+}
+
+// print the list of numbers as tab-separated
+void tsv( const vector<double> & nums ) {
+    dlm(nums,"\t");
+}
+
 // print the list of numbers as integers
-void Int( vector<double> & nums ) {
+void Int( const vector<double> & nums ) {
     for(auto i : nums) {
         cout << (int)i << endl;
     }
 }
 
 // find the max of a list of numbers
-double max( vector<double> & nums ) {
+double max( const vector<double> & nums ) {
     double out = nums[0];
     for(auto i : nums) {
         if(i > out) {
@@ -69,7 +81,7 @@ double max( vector<double> & nums ) {
 }
 
 // find the min of a list of numbers
-double min( vector<double> & nums ) {
+double min( const vector<double> & nums ) {
     double out = nums[0];
     for(auto i : nums) {
         if(i < out) {
@@ -80,7 +92,7 @@ double min( vector<double> & nums ) {
 }
 
 // output all numbers greater than the threshold
-void gt( vector<double> & nums, double thresh ) {
+void gt( const vector<double> & nums, const double & thresh ) {
     for(auto i : nums) {
         if(i > thresh) {
             cout << i << endl;
@@ -89,7 +101,7 @@ void gt( vector<double> & nums, double thresh ) {
 }
 
 // output all numbers greater than or equal to the threshold
-void gte( vector<double> & nums, double thresh ) {
+void gte( const vector<double> & nums, const double & thresh ) {
     for(auto i : nums) {
         if(i >= thresh) {
             cout << i << endl;
@@ -98,7 +110,7 @@ void gte( vector<double> & nums, double thresh ) {
 }
 
 // output all numbers less than the threshold
-void lt( vector<double> & nums, double thresh ) {
+void lt( const vector<double> & nums, const double & thresh ) {
     for(auto i : nums) {
         if(i < thresh) {
             cout << i << endl;
@@ -107,25 +119,34 @@ void lt( vector<double> & nums, double thresh ) {
 }
 
 // divide all numbers by num
-void div( vector<double> & nums, double num ) {
+void div( const vector<double> & nums, const double & num ) {
     for(auto i : nums) {
         cout << i/num << endl;
     }
 }
 
 // multiply all numbers by num
-void mult( vector<double> & nums, double num ) {
+void mult( const vector<double> & nums, const double & num ) {
     for(auto i : nums) {
         cout << i*num << endl;
     }
 }
 
 // output all numbers less than or equal to the threshold
-void lte( vector<double> & nums, double thresh ) {
+void lte( const vector<double> & nums, const double & thresh ) {
     for(auto i : nums) {
         if(i <= thresh) {
             cout << i << endl;
         }
+    }
+}
+
+// replace all instances of "search" with "replace" in "subject"
+void replace(string & subject, const string & search, const string & replace) {
+    size_t pos = 0;
+    while((pos = subject.find(search, pos)) != string::npos) {
+         subject.replace(pos, search.length(), replace);
+         pos += replace.length();
     }
 }
 
@@ -152,8 +173,20 @@ int main( int argc, char* argv[] ) {
     else if(strcmp(argv[1],"-csv") == 0) {
         csv(nums);
     }
-    else if(argv[1][1] == 'd' && argv[1][2] == 'i' && argv[1][3] == 'v') {
-        div(nums, stod(((string)argv[1]).substr(4)));
+    else if(argv[1][1] == 'd') {
+        if(argv[1][2] == 'i' && argv[1][3] == 'v') {
+            div(nums, stod(((string)argv[1]).substr(4)));
+        }
+        else if(argv[1][2] == 'l' && argv[1][3] == 'm') {
+            string delimiter = argv[1];
+            delimiter = delimiter.substr(4,delimiter.length()-4);
+            replace(delimiter,"\\a","\a");
+            replace(delimiter,"\\b","\b");
+            replace(delimiter,"\\n","\n");
+            replace(delimiter,"\\r","\r");
+            replace(delimiter,"\\t","\t");
+            dlm(nums,delimiter);
+         }
     }
     else if(argv[1][1] == 'g' && argv[1][2] == 't') {
         if(argv[1][3] == 'e') {
@@ -185,6 +218,9 @@ int main( int argc, char* argv[] ) {
     }
     else if(strcmp(argv[1],"-sum") == 0) {
         cout << sum(nums) << endl;
+    }
+    else if(strcmp(argv[1],"-tsv") == 0) {
+        tsv(nums);
     }
     else {
         cerr << "ERROR: Invalid argument: " << argv[1] << endl;
