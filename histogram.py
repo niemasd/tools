b20028ad40b137a5c0e43bb18712f084b7783cf0
Niema Moshiri <niemamoshiri@gmail.com>
2017-05-01 21:55:18 -0700

    Updated histogram

--- histogram.py
+++ histogram.py
@@ -1,6 +1,7 @@
 #! /usr/bin/env python
 from sys import stdin,argv
 import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
 import seaborn as sns
 if len(argv) not in {2,4}:
     print("ERROR: Incorrect number of arguments. Use -h argument for help")
@@ -10,10 +11,14 @@ if argv[1] in {'-h','-help','--help'}:
     print("USAGE: python histogram.py <title> <xlabel> <ylabel>")
     exit(0)
 data = [float(i) for i in stdin.read().strip().split()]
-sns.distplot(data, kde=False)
+fig, ax = plt.subplots()
+#ax.xaxis.set_major_locator(ticker.MaxNLocator(integer=True)) # integer ticks x axis
+#ax.yaxis.set_major_locator(ticker.MaxNLocator(integer=True)) # integer ticks y axis
+sns.distplot(data, kde=False) # kde=False removes the curve
 plt.title(argv[1])
 plt.xlabel(argv[2])
 plt.ylabel(argv[3])
-#plt.xlim(0,10) # Uncomment this and set (start,end) for x range
+#plt.xlim(0,10) # x range
+#plt.ylim(0,18) # y range
 plt.tight_layout()
 sns.plt.show()
