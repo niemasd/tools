b36cb2a8ec8305ede85a61a1d4c956544370b2e1
Niema Moshiri <niemamoshiri@gmail.com>
2017-05-02 14:12:18 -0700

    Updated histogram

--- histogram.py
+++ histogram.py
@@ -1,8 +1,5 @@
 #! /usr/bin/env python
 from sys import stdin,argv
-import matplotlib.pyplot as plt
-import matplotlib.ticker as ticker
-import seaborn as sns
 if len(argv) not in {2,4}:
     print("ERROR: Incorrect number of arguments. Use -h argument for help")
     exit(-1)
@@ -10,6 +7,10 @@ if argv[1] in {'-h','-help','--help'}:
     print("Creates histogram from list of numbers passed in via STDIN")
     print("USAGE: python histogram.py <title> <xlabel> <ylabel>")
     exit(0)
+
+import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
+import seaborn as sns
 data = [float(i) for i in stdin.read().strip().split()]
 fig, ax = plt.subplots()
 #ax.xaxis.set_major_locator(ticker.MaxNLocator(integer=True)) # integer ticks x axis
