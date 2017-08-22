ef6e302ac025b45c236ed61ee11bdd749e0da15f
Nima Moshiri <a1moshir@calab-fe.cluster>
2017-08-21 23:50:14 -0700

    Added fastq_merge.py

---
+++ fastq_merge.py
@@ -0,0 +1,16 @@
+#! /usr/bin/env python3
+'''
+Niema Moshiri 2017
+
+Merge multiple FASTQ files
+'''
+from sys import argv
+for f in argv[1:]:
+    name = '.'.join(f.split('/')[-1].split('.')[:-1])
+    i = 0
+    for line in open(f):
+        if i % 4 == 0: # header line
+            print("%s_%d" % (name,i/4))
+        else:
+            print(line.strip())
+        i += 1
