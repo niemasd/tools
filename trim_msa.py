0e2fb8a3b9fc5f445463f1d830fcf8f94a88dd30
Niema Moshiri <niemamoshiri@gmail.com>
2021-02-16 14:54:31 -0800

    Update trim_msa.py

--- trim_msa.py
+++ trim_msa.py
@@ -12,13 +12,15 @@ def parseArgs():
     parser = argparse.ArgumentParser(description=__doc__, formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument('-i', '--input', required=False, type=argparse.FileType('r'), default=stdin, help="Input FASTA")
     parser.add_argument('-o', '--output', required=False, type=argparse.FileType('w'), default=stdout, help="Output FASTA")
-    parser.add_argument('-s', '--start', required=False, type=int, default=1, help="Start Position (1-based)")
-    parser.add_argument('-e', '--end', required=False, type=int, default=None, help="End Position (1-based)")
+    parser.add_argument('-s', '--start', required=False, type=int, default=0, help="Length to Trim from Start")
+    parser.add_argument('-e', '--end', required=False, type=int, default=0, help="Length to Trim from End")
     args = parser.parse_args()
     return args.input, args.output, args.start, args.end
 
 # main code execution
 infile, outfile, start, end = parseArgs()
+assert start >= 0, "Length to trim from start must be non-negative integer"
+assert end >= 0, "Length to trim from end must be non-negative integer"
 curr_ID = None; curr_seq = None
 for line in infile:
     l = line.strip()
@@ -26,11 +28,11 @@ for line in infile:
         continue
     if l[0] == '>':
         if curr_ID is not None:
-            outfile.write("%s\n%s\n" % (curr_ID, curr_seq[start:end]))
+            outfile.write("%s\n%s\n" % (curr_ID, curr_seq[start:-end]))
         curr_ID = l; curr_seq = ''
     else:
         assert curr_seq is not None, "Invalid input FASTA"
         curr_seq += l
 if curr_ID is not None:
-    outfile.write("%s\n%s\n" % (curr_ID, curr_seq[start:end]))
+    outfile.write("%s\n%s\n" % (curr_ID, curr_seq[start:-end]))
 infile.close(); outfile.close()
