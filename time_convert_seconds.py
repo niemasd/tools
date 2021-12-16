070681130784b31d7c31eec55963a94e19d61b42
Niema Moshiri <niemamoshiri@gmail.com>
2021-12-15 16:28:56 -0800

    Support /usr/bin/time -v

--- time_convert_seconds.py
+++ time_convert_seconds.py
@@ -2,7 +2,7 @@
 '''
 Niema Moshiri 2021
 
-Convert ##m##s time to seconds
+Convert time from `time` command to seconds
 '''
 import argparse
 from sys import stdin
@@ -18,6 +18,17 @@ def parseArgs():
 for l in parseArgs():
     if len(l.strip()) == 0:
         continue
-    c1,c2 = [v.strip() for v in l.strip().split()]
-    m,s = [float(v) for v in c2.rstrip('s').split('m')]
-    print("%s\t%f" % (c1, 60*m + s))
+    end = l.split()[-1]
+    if "Elapsed (wall clock) time (h:mm:ss or m:ss):" in l:
+        parts = end.split(':')
+        if len(parts) == 3: # h:mm:ss
+            t = float(parts[0])*3600 + float(parts[1])*60 + float(parts[2])
+        elif len(parts) == 2: # m:ss
+            t = float(parts[0])*60 + float(parts[1])
+        else:
+            assert False, "Invalid time: %s" % ':'.join(parts)
+        print("Elapsed (wall clock) time (seconds): %s" % t)
+    elif 'm' in end: # `time` (0m0.000s)
+        c1,c2 = [v.strip() for v in l.strip().split()]
+        m,s = [float(v) for v in c2.rstrip('s').split('m')]
+        print("%s\t%f" % (c1, 60*m + s))
