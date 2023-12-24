29f3f2f0ae0c86ebc729b8b9b126021c6f38a9f7
Niema Moshiri <niemamoshiri@gmail.com>
2023-12-23 17:52:35 -1000

    Added script to split video file into chapters using ffmpeg

---
+++ split_chapters.py
@@ -0,0 +1,50 @@
+#! /usr/bin/env python3
+'''
+Use ffmpeg to split chapters of a given file
+'''
+
+# imports
+from os.path import abspath, expanduser, isfile
+from subprocess import run
+import argparse
+
+# constants
+EXTS = {'avi', 'mkv', 'mov', 'mp4', 'wmv'}
+
+# parse user args
+def parse_args():
+    parser = argparse.ArgumentParser(description=__doc__, formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('-i', '--input', required=True, type=str, help="Input File")
+    parser.add_argument('-p', '--prefix', required=False, type=str, default='', help="Output Prefix")
+    parser.add_argument('-cv', '--copy_video_codec', action='store_true', help="Copy Video Codec")
+    parser.add_argument('-ca', '--copy_audio_codec', action='store_true', help="Copy Audio Codec")
+    parser.add_argument('-q', '--quiet', action='store_true', help="Suppress Log Messages")
+    args = parser.parse_args()
+
+    # check args before returning
+    if not isfile(args.input):
+        raise ValueError("File not found: %s" % args.input)
+    if args.input.split('.')[-1].strip().lower() not in EXTS:
+        raise ValueError("Unsupported file extension: %s" % args.input)
+    args.input = abspath(expanduser(args.input))
+    return args
+
+# main execution
+if __name__ == "__main__":
+    args = parse_args()
+    input_ext = args.input.split('.')[-1].strip()
+    get_chapters_command = ['ffmpeg', '-i', args.input]
+    get_chapters_lines = run(get_chapters_command, capture_output=True).stderr.decode().split('  Chapters:')[1].splitlines()
+    chapter_times = [[float(t) for t in l.strip().split('start ')[1].split(', end ')] for l in get_chapters_lines if l.startswith('    Chapter #')]
+    for curr_ind, curr_times in enumerate(chapter_times):
+        start_time, end_time = curr_times
+        curr_fn = '%s%d.%s' % (args.prefix, curr_ind+1, input_ext)
+        curr_command = ['ffmpeg', '-i', args.input, '-ss', str(start_time), '-to', str(end_time)]
+        if args.copy_video_codec:
+            curr_command += ['-c:v', 'copy']
+        if args.copy_audio_codec:
+            curr_command += ['-c:a', 'copy']
+        curr_command += [curr_fn]
+        if not args.quiet:
+            print(' '.join(curr_command))
+        run(curr_command, capture_output=args.quiet)
